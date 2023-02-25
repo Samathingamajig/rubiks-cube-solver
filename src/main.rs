d1c7fe29d82f18b2a312c581a63a914bd30edb48
Samuel Gunter <samathingamajig@gmail.com>
2023-02-25 02:22:04 -0600

    Full face rotation, random face, color display

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fmt::{Debug, Formatter};
+use rand::prelude::*;
+use std::fmt::{Debug, Display, Formatter};
 
 struct RubiksCube {
     size: usize,
@@ -61,6 +62,46 @@ impl Debug for RubiksCube {
     }
 }
 
+impl Display for RubiksCube {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
+        fn output_single_row(fmt: &mut Formatter<'_>, row: &Vec<Color>) -> std::fmt::Result {
+            for c in row {
+                write!(fmt, "{c}")?
+            }
+            Ok(())
+        }
+
+        let leading_spaces = " ".repeat(self.faces[0].len() * 2);
+
+        for row in &self.faces[0][..] {
+            write!(fmt, "{leading_spaces}")?;
+            output_single_row(fmt, row)?;
+            writeln!(fmt)?;
+        }
+
+        for (((left, front), right), back) in self.faces[1][..]
+            .iter()
+            .zip(&self.faces[2][..])
+            .zip(&self.faces[3][..])
+            .zip(&self.faces[4][..])
+        {
+            output_single_row(fmt, left)?;
+            output_single_row(fmt, front)?;
+            output_single_row(fmt, right)?;
+            output_single_row(fmt, back)?;
+            writeln!(fmt)?;
+        }
+
+        for row in &self.faces[5][..] {
+            write!(fmt, "{leading_spaces}")?;
+            output_single_row(fmt, row)?;
+            writeln!(fmt)?;
+        }
+
+        Ok(())
+    }
+}
+
 #[derive(Clone, Copy)]
 enum Color {
     White,
@@ -71,7 +112,7 @@ enum Color {
     Green,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, num_derive::FromPrimitive, Debug)]
 enum Face {
     Up,
     Left,
@@ -111,6 +152,35 @@ impl Debug for Color {
                 Color::Orange => "O",
                 Color::Blue => "B",
                 Color::Green => "G",
+            }
+        )
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, fmt: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            fmt,
+            "{}",
+            match self {
+                Color::White => yansi::Paint::black("[]")
+                    .bg(yansi::Color::Fixed(255))
+                    .to_string(),
+                Color::Yellow => yansi::Paint::black("[]")
+                    .bg(yansi::Color::RGB(255, 255, 0))
+                    .to_string(),
+                Color::Red => yansi::Paint::black("[]")
+                    .bg(yansi::Color::RGB(255, 0, 0))
+                    .to_string(),
+                Color::Orange => yansi::Paint::black("[]")
+                    .bg(yansi::Color::RGB(255, 100, 0))
+                    .to_string(),
+                Color::Blue => yansi::Paint::black("[]")
+                    .bg(yansi::Color::RGB(0, 0, 255))
+                    .to_string(),
+                Color::Green => yansi::Paint::black("[]")
+                    .bg(yansi::Color::RGB(0, 140, 0))
+                    .to_string(),
             }
         )
     }
@@ -179,12 +249,14 @@ fn rotate_face(rc: &mut RubiksCube, face: Face, movement: Movement, depth: usize
         let s = rc.size - 1;
         match movement {
             Movement::Clockwise => {
-                for i in 0..s {
-                    let temp = main_face[0][i];
-                    main_face[0][i] = main_face[s - i][0];
-                    main_face[s - i][0] = main_face[s][s - i];
-                    main_face[s][s - i] = main_face[i][s];
-                    main_face[i][s] = temp;
+                for o in 0..(rc.size / 2) {
+                    for i in o..(s - o) {
+                        let temp = main_face[o][i];
+                        main_face[o][i] = main_face[s - i][o];
+                        main_face[s - i][o] = main_face[s - o][s - i];
+                        main_face[s - o][s - i] = main_face[i][s - o];
+                        main_face[i][s - o] = temp;
+                    }
                 }
             }
             Movement::CounterClockwise => todo!(),
@@ -229,10 +301,33 @@ fn main() {
     // rc.faces[Face::Front as usize][0][3] = Color::Green;
     // rc.faces[Face::Front as usize][0][4] = Color::White;
 
+    // rc.faces[Face::Front as usize][3][1] = Color::Yellow;
+    // rc.faces[Face::Front as usize][3][2] = Color::Orange;
+
     // println!("{rc:?}");
 
     // for _ in 0..8 {
-    //     rotate_face(&mut rc, Face::Front, Movement::Clockwise);
+    //     rotate_face(&mut rc, Face::Front, Movement::Clockwise, 0);
+    //     std::io::stdin().read_line(&mut String::new()).unwrap();
+
+    //     println!("{rc:?}");
+    // }
+
+    // let mut rc = RubiksCube::new(3);
+
+    // println!("{rc:?}");
+
+    // rc.faces[Face::Front as usize][0][0] = Color::Yellow;
+    // rc.faces[Face::Front as usize][0][1] = Color::Blue;
+    // rc.faces[Face::Front as usize][2][1] = Color::Red;
+
+    // rc.faces[Face::Left as usize][0][2] = Color::White;
+    // rc.faces[Face::Left as usize][2][2] = Color::Green;
+
+    // println!("{rc:?}");
+
+    // for _ in 0..8 {
+    //     rotate_face(&mut rc, Face::Front, Movement::Clockwise, 1);
     //     std::io::stdin().read_line(&mut String::new()).unwrap();
 
     //     println!("{rc:?}");
@@ -240,21 +335,15 @@ fn main() {
 
     let mut rc = RubiksCube::new(3);
 
-    println!("{rc:?}");
+    println!("{rc}");
 
-    rc.faces[Face::Front as usize][0][0] = Color::Yellow;
-    rc.faces[Face::Front as usize][0][1] = Color::Blue;
-    rc.faces[Face::Front as usize][2][1] = Color::Red;
-
-    rc.faces[Face::Left as usize][0][2] = Color::White;
-    rc.faces[Face::Left as usize][2][2] = Color::Green;
-
-    println!("{rc:?}");
-
-    for _ in 0..8 {
-        rotate_face(&mut rc, Face::Front, Movement::Clockwise, 1);
+    let mut rng = rand::thread_rng();
+    for _ in 0..20 {
+        let face = num_traits::FromPrimitive::from_u32(rng.gen_range(0..6)).unwrap();
+        rotate_face(&mut rc, face, Movement::Clockwise, 0);
         std::io::stdin().read_line(&mut String::new()).unwrap();
 
-        println!("{rc:?}");
+        println!("Moving face {face:?}");
+        println!("{rc}");
     }
 }
